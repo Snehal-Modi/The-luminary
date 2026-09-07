3de35cadad7a3ce4f9e10b6ab0a20165c60229e1
Snehal-Modi <modisnehal4790@gmail.com>
2026-09-07 16:59:57 +0530

    fix: wire rendering gallery into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navigation from './components/Navigation/Navigation.jsx'
 import Hero from './components/Hero/Hero.jsx'
 import ExperienceNav from './components/ExperienceNav/ExperienceNav.jsx'
+import RenderingGallery from './components/RenderingGallery/RenderingGallery.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import IntroSequence from './components/IntroSequence/IntroSequence.jsx'
 
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Hero />
         <ExperienceNav />
+        <RenderingGallery />
       </main>
       <Footer />
     </div>
